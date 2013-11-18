5daa8174816835cfbb5fb77f6756612b401a79e1
Chase Cox <coxchase@gmail.com>
2013-11-17 18:31:06 -0800

    Commiting

---
+++ HW6/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IS445.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Task2()
+        {
+            return View();
+        }
+
+        public ActionResult FormatPhoneNumber(string inputNumber)
+        {
+
+          if (string.IsNullOrEmpty(inputNumber))
+          {
+            return Content("invalid input, please try again");
+          }
+          else
+          {
+            string formatted = formatNumber(inputNumber);
+            return View((object)formatted);
+          }
+        }
+
+        // please implement your phone number formatting logic here
+        private string formatNumber(string inputNumber)
+        {
+          // imiplement formatting here
+
+            string finalstring = inputNumber;
+
+            inputNumber = String.Concat("(", finalstring.Substring(0, 3), ") ", finalstring.Substring(3, 3), "-", finalstring.Substring(6, 4));
+
+          return "formated number is: " + inputNumber;
+        }
+
+    }
+}
