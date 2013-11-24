36c841e52f9049eb97b4a453ec6b665b575b349f
Chase Cox <coxchase@gmail.com>
2013-11-24 09:33:15 -0800

    Commit

---
+++ HW7/Controllers/WebFormController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IS445.Controllers
+{
+    public class WebFormController : Controller
+    {
+        //
+        // GET: /Task2/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult WebForm()
+        {
+            return View();
+        }
+
+        public ActionResult Bio()
+        {
+            return View();
+        }
+    }
+}
